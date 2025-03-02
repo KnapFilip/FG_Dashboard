a2300489c433a2c3d908608cb1aa619a1f1e3397
Filip_Knap <KnapFilip@email.cz>
2025-03-02 01:10:18 +0100

    Refactor logout functionality; update session handling and redirect to external site

--- php/logout.php
+++ php/logout.php
@@ -1,2 +1,10 @@
 <?php
-session_destroy();
+// Zde ukončíme session
+session_start();
+session_unset(); // Smaže všechny proměnné session
+session_destroy(); // Zničí session
+
+// Přesměrování na jiný web
+header("Location: https://www.KnapF.eu");
+exit; // Ukončí skript, aby se přesměrování provedlo okamžitě
+?>
